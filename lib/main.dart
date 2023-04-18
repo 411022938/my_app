4e1ea1b8327bbf847bba7acf47d0bae359d044ec
411022938 <s1102293@gm.pu.edu.tw>
2023-04-19 05:25:35 +1000

    個人介紹

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '資管二A 陳霈哲個人app'),
     );
   }
 }
@@ -45,14 +45,15 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: const Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              style: TextStyle(fontSize: 40),
+              '我的名字叫陳霈哲，2X歲。家住海邊，未婚。我在大學為學分打拼。每天都要打破幾片平板才肯罷休。我不吸麻，酒僅止於淺嘗。晚上沒有在跟你睡覺的，每天只要睡足5個小時。睡前，我一定喝一杯熱咖啡，然後打2小時的osu，上了床，馬上打貓戰。一覺到天暗，絕不把疲勞和壓力，留給突襲時間。朋友都說我很正常。',
             ),
-          ],
+          ),
         ),
       ),
     );
